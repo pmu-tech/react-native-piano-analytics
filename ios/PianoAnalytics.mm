df5f53d823420ee4530e8a92768f37d43426bad7
Emilie SENG <emilie.seng.frontguys@ext.pmu.fr>
2023-10-11 11:26:34 +0200

    feat: add ios visitor id functions

--- ios/PianoAnalytics.mm
+++ ios/PianoAnalytics.mm
@@ -23,6 +23,10 @@ RCT_EXTERN_METHOD(privacyGetMode: (RCTPromiseResolveBlock)resolve reject:(RCTPro
 RCT_EXTERN_METHOD(setUser: (NSString)userId category:(NSString)category enableStorage:(BOOL)enableStorage);
 RCT_EXTERN_METHOD(deleteUser);
 
+// PRIVACY ID
+RCT_EXTERN_METHOD(setVisitorId: (NSString)visitorId);
+RCT_EXTERN_METHOD(getVisitorId: (RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject);
+
 // Don't compile this code when we build for the old architecture.
 #ifdef RCT_NEW_ARCH_ENABLED
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
