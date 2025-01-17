68bc7543c94e9efb997597b1b2f4235ebdb4d701
ZAIEM Houssem-Eddine <houssem-eddine.zaiem@pmu.fr>
2025-01-17 19:20:31 +0100

    fix: code gen on new architecture projects was not working

--- ios/PianoAnalytics.h
+++ ios/PianoAnalytics.h
@@ -1,11 +1,12 @@
-
 #ifdef RCT_NEW_ARCH_ENABLED
 #import "RNPianoAnalyticsSpec.h"
-
-@interface RNPianoAnalytics : NSObject <NativePianoAnalyticsSpec>
 #else
 #import <React/RCTBridgeModule.h>
+#endif
 
+#ifdef RCT_NEW_ARCH_ENABLED
+@interface RNPianoAnalytics : NSObject <NativePianoAnalyticsSpec>
+#else
 @interface RNPianoAnalytics : NSObject <RCTBridgeModule>
 #endif
 
